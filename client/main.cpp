0df2cbdc0c60d424841320043947fe669748c60d
ZhengWangang <dogonthetree@gmail.com>
2019-07-12 17:09:59 +0800

    删除无用代码

--- client/main.cpp
+++ client/main.cpp
@@ -1,44 +1,7 @@
 //
 // Created by zwg on 19-7-10.
 //
-
 #include "main.hpp"
-
-/*void testTps(char* argv[]) {
-    const char* host = argv[2];
-    const char* port = argv[3];
-    const char* chain_id = argv[4];
-    const char* user1 = argv[5];
-    const char* user1PK = argv[6];
-    const char* user2 = argv[7];
-    const char* user2PK = argv[8];
-    const char* tokenName = argv[9];
-    const char* contractName = argv[10];
-    uint64_t period = static_cast<uint64_t>(atol(argv[11]));
-    uint32_t eachTime = static_cast<uint32_t>(atoi(argv[12]));
-    while(true) {
-        auto& ioc = IOC::app();
-        Client client(
-                ioc, host, port, chain_id,
-                user1, user1PK, user2, user2PK,
-                tokenName, contractName, period, eachTime);
-        ioc.run();
-        cout << "Reconnect." << endl;
-    }
-}
-
-void stat(char* argv[]) {
-    const char* host = argv[2];
-    const char* port = argv[3];
-    const char* grafanaIP = argv[4];
-    const char* grafanaPort = argv[5];
-    while(true) {
-        auto& ioc = IOC::app();
-        Client client(ioc, host, port, grafanaIP, grafanaPort);
-        ioc.run();
-        cout << "Reconnect." << endl;
-    }
-}*/
 
 int main(int argc, char* argv[]) {
     if(argc != 2 or strcmp(argv[1], "-h") == 0 or strcmp(argv[1], "--help") == 0) {
