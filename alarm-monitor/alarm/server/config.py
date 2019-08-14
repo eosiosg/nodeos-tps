f7cef08444667ad134f763487ded8acf57367a69
ZhengWangang <dogonthetree@gmail.com>
2019-08-14 11:01:47 +0800

    按服务设置报警

--- alarm-monitor/alarm/server/config.py
+++ alarm-monitor/alarm/server/config.py
@@ -1,7 +1,24 @@
 # 请联系郑万刚获取
-ALI_ACCESS_KEY_ID = ''
-ALI_ACCESS_KEY_SECRET = ''
-ALI_CALLER = ''
+# 新增报警类型需要操作的步骤:
+#   1. 添加"alarm type from client";
+#   2. 在alarm_service_name_map中添加对应的项;
+#   3. 在PHONE_NUMBER_DICT中添加对应的项;
+
+# error code to client
+SUCCESS = 0
+PARAMETER_ERROR = 1
+AUTH_FAILED_ERROR = 2
+INVALID_TYPE_ERROR = 3
+
+# alarm type from client
+HEAD_LIB_DIFF_HIGH = 1
+
+
+# alarm message to user
+alarm_service_name_map = {
+    HEAD_LIB_DIFF_HIGH: 'Head-lib-diff',
+}
+
 
 TWILIO_ACCESS_KEY_ID = ''
 TWILIO_ACCESS_KEY_SECRET = ''
@@ -9,5 +26,9 @@ TWILIO_CALLER = ''
 
 AUTH_CODE = ''
 
-PHONE_NUMBER_LIST = [
-]
+
+PHONE_NUMBER_DICT = {
+    # alarm_type : [phone_number_1, phone_number_2,]),
+    HEAD_LIB_DIFF_HIGH: [],
+
+}
--- alarm-monitor/alarm/server/server.py
+++ alarm-monitor/alarm/server/server.py
@@ -5,41 +5,26 @@ from twilio.rest import Client as TwilioClient
 
 app = Flask(__name__)
 
-# error code to client
-SUCCESS = 0
-PARAMETER_ERROR = 1
-AUTH_FAILED_ERROR = 2
-INVALID_TYPE_ERROR = 3
-
-# alarm type from client
-HEAD_LIB_DIFF_HIGH = 1
-
-# alarm message to user
-alarm_service_name_map = {
-    HEAD_LIB_DIFF_HIGH: ('Head-lib-diff', 'head和lib的差值过大')
-}
-
 # key: 'type:{type}:node:{node}', value: last send time
-last_notify_time_map = {
-    HEAD_LIB_DIFF_HIGH: 0.0
-}
+last_notify_time_map = dict()
 
 twilio_client = TwilioClient(TWILIO_ACCESS_KEY_ID, TWILIO_ACCESS_KEY_SECRET)
 
 
 def notify(type_, node, extra=None):
-    body = 'service name:{}, \nnode:{}'.format(alarm_service_name_map[type_][0], node)
+    body = 'service name:{}, \nnode:{}'.format(alarm_service_name_map[type_], node)
     if extra is not None:
         body += ' ,\nextra:{}.\n'.format(extra)
-    for phone in PHONE_NUMBER_LIST:
+
+    for phone in PHONE_NUMBER_DICT[type_]:
         twilio_client.messages.create(
             body=body,
             from_=TWILIO_CALLER,
-            to=phone[1])
+            to=phone)
     print(body)
 
 
-@app.route('/alarm_update', methods=['POST'])
+@app.route('/alarm_upload', methods=['POST'])
 def alarm_update():
     req_data = request.get_json()
     print(req_data)
@@ -47,17 +32,17 @@ def alarm_update():
         return jsonify({"code": PARAMETER_ERROR})
     if req_data['code'] != AUTH_CODE:
         return jsonify({"code": AUTH_FAILED_ERROR})
-    if req_data['type'] not in last_notify_time_map:
+    if req_data['type'] not in alarm_service_name_map:
         return jsonify({"code": INVALID_TYPE_ERROR})
     if 'extra' in req_data:
         extra = req_data['extra']
     else:
         extra = None
     now = time()
-    key = 'type:{}:node:{}'.format(req_data['type'], req_data['node'])
-    if key not in last_notify_time_map or now - last_notify_time_map[key] > 60:
+    k = 'type:{}:node:{}'.format(req_data['type'], req_data['node'])
+    if k not in last_notify_time_map or now - last_notify_time_map[k] > 60:
         notify(req_data['type'], req_data['node'], extra)
-        last_notify_time_map[key] = now
+        last_notify_time_map[k] = now
 
     return jsonify({"code": SUCCESS})
 
