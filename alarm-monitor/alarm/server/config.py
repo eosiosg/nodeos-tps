29ec28f0d3ccc15e7170c5a75d6e79e802e091b4
ZhengWangang <dogonthetree@gmail.com>
2019-08-14 15:19:30 +0800

    添加WPS、IBC、ElasticSearch上报错误码

--- alarm-monitor/alarm/server/config.py
+++ alarm-monitor/alarm/server/config.py
@@ -11,12 +11,19 @@ AUTH_FAILED_ERROR = 2
 INVALID_TYPE_ERROR = 3
 
 # alarm type from client
+TEST_ALARM = 0
 HEAD_LIB_DIFF_HIGH = 1
-
+IBC_ALARM = 2
+WPS_ALARM = 3
+ES_ALARM = 4
 
 # alarm message to user
 alarm_service_name_map = {
+    TEST_ALARM: 'test-alarm',
     HEAD_LIB_DIFF_HIGH: 'Head-lib-diff',
+    IBC_ALARM: 'ibc-alarm',
+    WPS_ALARM: 'wps-alarm',
+    ES_ALARM: 'es-hypersion-alarm',
 }
 
 
@@ -26,9 +33,14 @@ TWILIO_CALLER = ''
 
 AUTH_CODE = ''
 
+pn = {
+}
 
 PHONE_NUMBER_DICT = {
     # alarm_type : [phone_number_1, phone_number_2,]),
+    TEST_ALARM:                    [],
     HEAD_LIB_DIFF_HIGH: [],
-
+    IBC_ALARM:          [],
+    WPS_ALARM:          [],
+    ES_ALARM:           [],
 }
